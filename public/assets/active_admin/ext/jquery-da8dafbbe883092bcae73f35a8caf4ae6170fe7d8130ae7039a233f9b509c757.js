5be3bde37ac039c54267b1d815d08117639e3d18
Hugi <hugiasgeirsson@gmail.com>
2016-04-15 15:14:16 +0200

    precompiled js

---
+++ public/assets/active_admin/ext/jquery-da8dafbbe883092bcae73f35a8caf4ae6170fe7d8130ae7039a233f9b509c757.js
@@ -0,0 +1,13 @@
+(function() {
+  $.fn.serializeObject = function() {
+    var i, len, o, obj, ref;
+    obj = {};
+    ref = this.serializeArray();
+    for (i = 0, len = ref.length; i < len; i++) {
+      o = ref[i];
+      obj[o.name] = o.value;
+    }
+    return obj;
+  };
+
+}).call(this);
