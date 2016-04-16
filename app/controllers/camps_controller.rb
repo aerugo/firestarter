bbb8237b06e33cd3d648bd04de169502bf0399c6
Hugi <hugiasgeirsson@gmail.com>
2016-04-16 17:56:45 +0200

    dreams in creator name order

--- app/controllers/camps_controller.rb
+++ app/controllers/camps_controller.rb
@@ -2,7 +2,7 @@ class CampsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
 
   def index
-    @camps = Camp.all.order("RANDOM()").page(params[:page])
+    @camps = Camp.all.order(:contact_name).page(params[:page])
   end
 
   def new
