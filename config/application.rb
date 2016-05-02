c98d52b35e6030447e6d58b871949ee7a8a8fad9
KHJ <kasperhelweg@gmail.com>
2016-05-03 00:12:50 +0200

    add migrations and stub form

--- config/application.rb
+++ config/application.rb
@@ -23,15 +23,5 @@ module Firestarter
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     Rails.application.config.x.firestarter_settings = config_for(:firestarter_settings)
-
-    # S3 config
-    config.paperclip_defaults = {
-      :storage => :s3,
-      :s3_credentials => {
-        :bucket => ENV['S3_BUCKET_NAME'],
-        :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-        :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-      }
-    }
   end
 end
