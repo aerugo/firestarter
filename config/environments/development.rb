a0af3c0a9e39a3234cc598f6f91995afe12fa183
KHJ <kasperhelweg@gmail.com>
2016-05-03 17:27:10 +0200

    remove S3 from dev for now...

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,15 +45,4 @@ Rails.application.configure do
   # CONfiguration to use mailcatcher in development mode
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
-
-  # S3 config
-  config.paperclip_defaults = {
-    :storage => :s3,
-    :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-    }
-  }
-
 end
