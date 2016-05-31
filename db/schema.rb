56c72e1c3a2eee3b1ff6c9c9091ab6cc2f41d514
Hugi <hugiasgeirsson@gmail.com>
2016-05-31 23:39:01 +0200

    small fixes according to specifications

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160526084756) do
+ActiveRecord::Schema.define(version: 20160531085926) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -80,6 +80,11 @@ ActiveRecord::Schema.define(version: 20160526084756) do
     t.string   "instagram_link"
     t.string   "twitter_link"
     t.string   "video_link"
+    t.string   "address"
+    t.string   "start_time"
+    t.string   "end_time"
+    t.string   "organizer"
+    t.string   "benefit"
   end
 
   add_index "camps", ["user_id"], name: "index_camps_on_user_id"
