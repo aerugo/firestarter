295ce8f7958614f0fae2eb5a44e99bc0c4e2ce92
Hugi <hugiasgeirsson@gmail.com>
2016-09-16 02:51:20 +0000

    add confirmation

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160601150347) do
+ActiveRecord::Schema.define(version: 20160916023625) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -135,8 +135,13 @@ ActiveRecord::Schema.define(version: 20160601150347) do
     t.boolean  "guide",                  default: false
     t.boolean  "admin",                  default: false
     t.integer  "grants",                 default: 10
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
