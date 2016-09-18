a073072a7460c70c1adb3dc4c607a92a48f2b710
Hugi <hugiasgeirsson@gmail.com>
2016-09-18 11:28:23 +0000

    added owner to model, changed form texts

--- app/models/camp.rb
+++ app/models/camp.rb
@@ -4,6 +4,7 @@ class Camp < ActiveRecord::Base
 	has_many :memberships
 	has_many :users, through: :memberships
   has_many :images #, :dependent => :destroy
+	has_one :owner
 
 	validates :creator, presence: true
 	validates :name, presence: true
