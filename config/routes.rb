ea5e99edf59837d7fd137f9bc99ca4dc57279d8f
Hugi Ásgeirsson <hugiasgeirsson@gmail.com>
2017-06-15 16:16:26 +0200

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,21 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+  
   root 'camps#index'
+  
   devise_for :users,
-    :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  resources :camps do
+    controllers: { 
+      omniauth_callbacks: 'users/omniauth_callbacks',
+      registrations: 'users/registrations' 
+  }
+  
+  resources :camps, :path => 'aktiviteter' do
     resources :images
+    post 'camps/:id/join' => 'camps#join'
+    patch 'camps/:id/update_grants' => 'camps#update_grants'
+    patch 'camps/:id/toggle_granting' => 'camps#toggle_granting'
   end
-
-  post 'camps/:id/join' => 'camps#join'
-  patch 'camps/:id/update_grants' => 'camps#update_grants'
-  patch 'camps/:id/toggle_granting' => 'camps#toggle_granting'
 
   get '/chronicles' => 'chronicles#index'
 
